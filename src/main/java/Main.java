2e4e4cce7a37988a0515701a3fe1cb62519d5f22
Berk Can Arslan <berkcannarslann@outlook.com>
2023-12-01 01:57:46 +0300

    GUI Implementation
    Try to implement JavaFX to our Java project. Using Maven. It was hard to implement it. But it worked. Now we are going to try some modification.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        SQLConnection connection = new SQLConnection();
-        connection.getDatabaseConnection();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Example");
+
+        Button btn = new Button("Click me!");
+        btn.setOnAction(e -> System.out.println("Hello, JavaFX!"));
+
+        Scene scene = new Scene(btn, 300, 200);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 }
