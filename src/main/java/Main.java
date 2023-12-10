35725d01e3bd68ce3d64b9a612306a5ce5ba059c
Berk Can Arslan <berkcannarslann@outlook.com>
2023-12-10 17:20:23 +0300

    Revert "User Manual and Stage Icons Implementation"
    This reverts commit 4e96c1535e7f4ccf2cae54620c2af1473abdd67f.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -13,8 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
        Parent root = FXMLLoader.load(getClass().getResource("JavaFxController.fxml"));
-        primaryStage.getIcons().add(new Image("pngwing.com.png"));
-        primaryStage.setTitle("Syllabus System");
+       primaryStage.setTitle("Syllabus System");
        primaryStage.setScene(new Scene(root));
        primaryStage.show();
     }
