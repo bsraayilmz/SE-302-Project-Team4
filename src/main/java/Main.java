4e96c1535e7f4ccf2cae54620c2af1473abdd67f
Berk Can Arslan <berkcannarslann@outlook.com>
2023-12-10 14:14:54 +0300

    User Manual and Stage Icons Implementation
    User manual changed and stage icons changed.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -12,7 +13,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
        Parent root = FXMLLoader.load(getClass().getResource("JavaFxController.fxml"));
-       primaryStage.setTitle("Syllabus System");
+        primaryStage.getIcons().add(new Image("pngwing.com.png"));
+        primaryStage.setTitle("Syllabus System");
        primaryStage.setScene(new Scene(root));
        primaryStage.show();
     }
